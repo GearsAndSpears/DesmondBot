1de994f999ee1ba102d142ab2c319e2b8f17b3b5
mpinckney19 <44166116+mpinckney19@users.noreply.github.com>
2018-10-24 08:25:52 -0400

    Working Autonomous, weird problems with CV

--- ftc_app-4.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FacingCraterV2.java
+++ ftc_app-4.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FacingCraterV2.java
@@ -328,7 +328,7 @@ public class FacingCraterV2 extends LinearOpMode {
     }
 
     /**
-     *  Method to obtain & hold a heading for a finite amount of time
+     *  Method to obtain & hold a heading for a finite amount of tim
      *  Move will stop once the requested time has elapsed
      *
      * @param speed      Desired speed of turn.
